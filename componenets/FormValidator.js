60b702aae6733e48b7d48871cebaf57d2bbec36f
fauxfox117 <SABolin1@me.com>
2025-07-03 21:18:19 -0400

    FormValidator start

--- componenets/FormValidator.js
+++ componenets/FormValidator.js
@@ -1,13 +1,11 @@
 class FormValidator {
   constructor(settings, formElement) {
-    this._formSelector = settings._formSelector;
-    this.formElement = formElement;
-    this._formSelector: ;
-    this._inputSelector: ;
-    this._submitButtonSelector: ;
-    this._errorClass: ;
-    this._inputErrorClass: ;
-    this._inactiveButtonClass: ;
+    this._formSelector = settings.formSelector;
+    this._inputSelector = settings.inputSelector;
+    this._submitButtonSelector = settings.submitButtonSelector;
+    this._errorClass = settings.errorClass;
+    this._inputErrorClass = settings.inputErrorClass;
+    this._inactiveButtonClass = settings.inactiveButtonClass;
   }
 
   enableValidation() {}
