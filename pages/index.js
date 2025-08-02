8dc02096921ba62e9b4a9ef62c4c605fe646e1bd
fauxfox117 <SABolin1@me.com>
2025-08-02 13:43:53 -0400

    section class started

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,15 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
+import Section from "../components/Section.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
 const addTodoForm = document.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
 const todosList = document.querySelector(".todos__list");
+
+const section = new Section();
 
 const openModal = (modal) => {
   modal.classList.add("popup_visible");
