5ac56398397ad4a468ef6d03bcf974825ee5b9c3
fauxfox117 <SABolin1@me.com>
2025-08-06 23:51:55 -0400

    esc function working

--- components/Popup.js
+++ components/Popup.js
@@ -4,12 +4,21 @@ class Popup {
     this._popupCloseBtn = this._popupElement.querySelector(".popup__close");
   }
 
+  _handleEscapeClose(evt) {
+    // console.log("Escape key pressed, event:", evt);
+    if (evt.key === "Escape") this.close();
+  }
+
   open() {
     this._popupElement.classList.add("popup_visible");
+    document.addEventListener("keyup", (evt) => this._handleEscapeClose(evt));
   }
 
   close() {
     this._popupElement.classList.remove("popup_visible");
+    document.removeEventListener("keyup", (evt) =>
+      this._handleEscapeClose(evt)
+    );
   }
 
   setEventListeners() {
